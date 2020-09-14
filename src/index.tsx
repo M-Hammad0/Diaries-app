15c773eda74c95c636803959afcfbe7e30d6178d
unknown <hammad_205@hotmail.com>
2020-09-15 00:37:08 +0500

    add store and root reducer

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { setupServer } from './services/mirage/server';
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 
 if (process.env.NODE_ENV === 'development') {
   setupServer();
@@ -12,7 +16,9 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
